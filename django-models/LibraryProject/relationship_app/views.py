8eeffde3d6199b296792d3dfcb4598822d4c4b08
Tanson Jude Antwi <tanson.aj@gmail.com>
2025-08-03 01:40:22 +0000

    .detail added to DetailView import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 from .models import Book
 from .models import Library
